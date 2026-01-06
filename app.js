c466ee2c9ec68332cb21d768a433ffb6c435b1e2
AyushX1602 <ayushpathak16022005@gmail.com>
2026-01-06 15:03:52 +0530

    Setup and integration

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const path = require("path");
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));    
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
